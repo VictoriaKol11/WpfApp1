848552993616f1247bf2903be77854cc9b648b1c
VictoriaKol11 <kolvika11@gmail.com>
2022-05-16 16:36:17 +0600

    1 commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public string _synchonizedText;
+        public string SynchonizedText
+        {
+            get => _synchonizedText;
+            set
+            {
+                _synchonizedText = value;
+                OnPropertyChanged(nameof(SynchonizedText));
+            }
+        }
+    }
+}
